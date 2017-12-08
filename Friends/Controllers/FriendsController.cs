64365e9874c5fdabbe0188fe0d077018fc7811b4
imnbwd <nbwd@qq.com>
2017-12-08 15:19:54 +0800

    Add project files.

---
+++ Friends/Controllers/FriendsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Friends.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Friends")]
+    public class FriendsController : Controller
+    {
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        // GET: api/Friends
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Friends/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Friends
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Friends/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+    }
+}
---
+++ Friends/Models/Friend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friends.Models
+{
+    public class Friend
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+
+        public bool IsDeveloper { get; set; }
+    }
+}
